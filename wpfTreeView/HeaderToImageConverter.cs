46f7e25f6cc557450143ff60ca1f55864d0113e3
Abel <abelmasila@gmail.com>
2017-04-13 14:09:17 +0300

    added valueConverter class

---
+++ wpfTreeView/HeaderToImageConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace wpfTreeView
+{
+    [ValueConversion(typeof(string),typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+        /// <summary>
+        /// converts a fullpath into a specific image type of a drive,folder or file
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var path = (string)value;
+            if (path == null)
+                return null;
+            var image = "Images/file.png";
+            //get file name/folder
+            var name = MainWindow.GetFileFolderName(path);
+
+            //if name is blank, we assume its drive
+            if (string.IsNullOrEmpty(name))
+            {
+                image = "Images/drive.png";
+            }
+            else if(new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+            {
+                image = "Images/folder-closed.png";
+            }
+           
+
+            return new BitmapImage(new Uri($" pack:// application:,,,/{image}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        } 
+    }
+}
